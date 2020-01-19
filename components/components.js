8fc06b8bfe6c29a3eba1f7f9a32c84cf19ef57d6
jdunn010 <james_dunn2@cable.comcast.com>
2020-01-18 23:00:47 -0500

    updated POST method for user rotation data

--- components/components.js
+++ components/components.js
@@ -1,21 +1,58 @@
 // CURSOR-COMPONENT
 AFRAME.registerComponent('updatecubes', {
+    init: function(){
+        setInterval(() => {
+            const params = new URLSearchParams(window.location.search);
+            const userId = params.get("user-id");
+            if(userId){
+                console.log('user id is:', userId);
+                let postUrl = "https://aswnjqe5rb.execute-api.us-east-2.amazonaws.com/GA/users";
+                var cameraEl = document.querySelector('#camera').object3D;
+                var data = {
+                        'user-id': userId,
+                        'user-rotation': {x: cameraEl.rotation.x, y: cameraEl.rotation.y, z: cameraEl.rotation.z},
+                        'user-rotation-x':cameraEl.rotation.x,
+                        'user-rotation-y':cameraEl.rotation.y,
+                        'user-rotation-z':cameraEl.rotation.z
+                    };
+                $.ajax({
+                    type: 'POST',
+                    data: JSON.stringify(data),
+                    contentType: 'application/json',
+                    url: postUrl,
+                    success: function(jsondata){
+                        console.log('sending post data:', jsondata);
+                    },
+		    error: function(e) {
+			console.log('ERROR posting data:', e);
+                    }
+                })
+            }
+        }, 800);
+    },
     tick: function () {
 
+        let ease = 0.3;
+
+        let cubeDistance = 0.3;
         let cube = document.querySelector('#cube0').object3D;
         let nextCube = document.querySelector('#cube1').object3D;
-        cube.position.x = 0.8 * cube.position.x + (0.2 * nextCube.position.x + 0.08);
-        cube.position.y = 0.8 * cube.position.y + 0.2 * nextCube.position.y;
-        cube.position.z = 0.8 * cube.position.z + 0.2 * nextCube.position.z;
-        // TweenMax.to(nextCube.position, 1, {x: cube.position.x + 0.5, y: cube.position.y, z: cube.position.z});
+        cube.position.x = (1-ease) * cube.position.x + ease * (nextCube.position.x + cubeDistance);
+        cube.position.y = (1-ease) * cube.position.y + ease * nextCube.position.y;
+        cube.position.z = (1-ease) * cube.position.z + ease * nextCube.position.z;
+
+        // gsap.to(nextCube.position, {x: cube.position.x + cubeDistance, y: cube.position.y, z: cube.position.z, duration: 0.3});
+        // TweenMax.to(nextCube.position, 1, {x: cube.position.x + cubeDistance, y: cube.position.y, z: cube.position.z});
 
         cube = document.querySelector('#cube1').object3D;
         nextCube = document.querySelector('#cube2').object3D;
-        cube.position.x = 0.8 * cube.position.x + (0.2 * nextCube.position.x + 0.08);
-        cube.position.y = 0.8 * cube.position.y + 0.2 * nextCube.position.y;
-        cube.position.z = 0.8 * cube.position.z + 0.2 * nextCube.position.z;
-        // TweenMax.to(nextCube.position, 1, {x: cube.position.x + 0.5, y: cube.position.y, z: cube.position.z});
 
+        cube.position.x = (1-ease) * cube.position.x + ease * (nextCube.position.x + cubeDistance);
+        cube.position.y = (1-ease) * cube.position.y + ease * nextCube.position.y;
+        cube.position.z = (1-ease) * cube.position.z + ease * nextCube.position.z;
+
+        // gsap.to(nextCube.position, {x: cube.position.x + cubeDistance, y: cube.position.y, z: cube.position.z, duration: 0.3});
+        // TweenMax.to(nextCube.position, 1, {x: cube.position.x + cubeDistance, y: cube.position.y, z: cube.position.z});
     }
 });
 
